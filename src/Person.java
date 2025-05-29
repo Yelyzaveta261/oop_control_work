26cf63ded0f9a64c0830f910f14d0e17e4bea6f4
PC <lytvynenko.yelyzaveta@st.nkpk.op.edu.ua>
2025-05-29 13:59:24 +0200

    Initial commit with Person class

---
+++ src/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    // Атрибути
+    String name;
+    int age;
+
+    // Метод 1
+    public void sayHello() {
+        System.out.println("Hello! My name is " + name);
+    }
+
+    // Метод 2
+    public int getAgeNextYear() {
+        return age + 1;
+    }
+}
